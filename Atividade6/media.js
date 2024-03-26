21a3cfa8c6e410ad4fe217f7bb10e4409cd61cd2
jesleysales <115020737+jesleysales@users.noreply.github.com>
2024-03-26 10:57:14 -0300

    Add files via upload

---
+++ Atividade6/media.js
@@ -0,0 +1,11 @@
+var n1 = prompt("Digite a nota 1: ");
+var n2 = prompt("Digite a nota 2: ");
+var n3 = prompt("Digite a nota 3: ");
+
+var nota1 = parseFloat(n1);
+var nota2 = parseFloat(n2);
+var nota3 = parseFloat(n3);
+
+var media = (nota1 + nota2 + nota3) / 3;
+
+alert("A média é: " + media);
---
+++ Atividade6/operacoes.js
@@ -0,0 +1,11 @@
+var n1 = prompt("Número 1: ");
+var n2 = prompt("Número 2: ");
+
+var num1 = parseFloat(n1);
+var num2 = parseFloat(n2);
+
+alert("Soma: " + (num1 + num2)+
+    "\nSubtração: " + (num1 - num2)+
+    "\nProduto: " + (num1 * num2)+
+    "\nDivisão: " + (num1 / num2)+
+    "\nResto da divisão: " + (num1 % num2))
